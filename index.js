98e1fdd332ca480ac51c71a1f9308f50db3b3edc
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-05-06 14:00:18 -0400

    figuring out assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+const unmodifiedTestArr = [1, 2, 3, 4];
+const unmodifiedTestObj = { one: 1, two: 2, three: 3, four: 4 };
+const testArr = [1, 2, 3, 4];
+const testObj = Object.assign({}, unmodifiedTestObj);
+
+
+
+function myEach(collection, callback) { 
+    console.log(collection)
+    console.log(callback)
+}
+    
+
+// for (let i=0; i < collection.length ; i++)
+//     console.log(i)
+// }
+
+
+
+
